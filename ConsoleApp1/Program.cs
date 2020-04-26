808018776f91369e10662e55d68a6c2ec3bbe193
Arek Stepnik <arkadiusz.stepnik88@gmail.com>
2020-04-26 12:25:27 +0200

    itextadded

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            PdfWriter pdfWriter = new PdfWriter("hello.pdf");
+            PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+            Document document = new Document(pdfDocument);
+            document.Add(new Paragraph("Hello World!"));
+
         }
     }
 }
