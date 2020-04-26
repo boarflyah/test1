32ef4f9dc042803f8ee9d0e525063f8a0aeac56d
Arek Stepnik <arkadiusz.stepnik88@gmail.com>
2020-04-26 12:27:05 +0200

    itext2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,11 +13,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            PdfWriter pdfWriter = new PdfWriter("hello.pdf");
-            PdfDocument pdfDocument = new PdfDocument(pdfWriter);
-            Document document = new Document(pdfDocument);
-            document.Add(new Paragraph("Hello World!"));
-
+            using (PdfWriter pdfWriter = new PdfWriter("hello.pdf"))
+            using (PdfDocument pdfDocument = new PdfDocument(pdfWriter))
+            using (Document document = new Document(pdfDocument))
+            {
+                document.Add(new Paragraph("Hello World!"));
+            }
         }
     }
 }
